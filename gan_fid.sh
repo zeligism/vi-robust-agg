53e3b0a971d0db1fbe16791a3b9a8895ed3d9f65
Abdulla Almansoori <abdulla.almansoori@mbzuai.ac.ae>
2023-03-15 23:04:30 +0400

    .

--- gan_fid.sh
+++ gan_fid.sh
@@ -6,6 +6,7 @@ dataset_stats="datasets/cifar-10-stats.npz"
 test_exp_dir="outputs/gan/SGDA_CC/n10_f2_avg_ALIE_lr0.001_seed0_wsteps4"
 
 for exp_dir in outputs/gan/*/*; do
+    echo
     echo ${exp_dir}
     # Generate samples
     python run_gan_samples.py --use-cuda --num-samples ${num_samples} --model-path "${exp_dir}/gan_output/model_epoch${epochs}.pl"
