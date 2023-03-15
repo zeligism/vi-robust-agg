8791cd89236d04bbba59fe933b9aef3504736dfb
Abdulla Almansoori <abdulla.almansoori@mbzuai.ac.ae>
2023-03-15 21:20:57 +0400

    merged FID scripts

--- gan_fid.sh
+++ gan_fid.sh
@@ -1,8 +1,16 @@
+num_samples=10000
+epochs=50
 cuda="--device cuda:0"
 #cuda=""
 dataset_stats="datasets/cifar-10-stats.npz"
-for samples_dir in outputs/gan/*/*/gan_output/fake_data; do
-    #test_samples_dir="outputs/gan/SGDA_CC/n10_f2_avg_ALIE_lr0.001_seed0_wsteps4/gan_output/fake_data"
-    echo ${samples_dir}
-    python -m pytorch_fid ${cuda} ${dataset_stats} ${samples_dir}
+test_exp_dir="outputs/gan/SGDA_CC/n10_f2_avg_ALIE_lr0.001_seed0_wsteps4"
+
+for exp_dir in outputs/gan/*/*; do
+    echo ${exp_dir}
+    # Generate samples
+    python run_gan_samples.py --use-cuda --num-samples ${num_samples} --model-path "${exp_dir}/gan_output/model_epoch${epochs}.pl"
+    # Calculate FID
+    python -m pytorch_fid ${cuda} ${dataset_stats} "${exp_dir}/gan_output/fake_data"
+    # remove samples dir
+    rm -rf "${exp_dir}/gan_output/fake_data"
 done
